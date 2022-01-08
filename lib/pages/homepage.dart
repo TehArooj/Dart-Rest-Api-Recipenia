3ae56599e43f8b47b8032bbb35afdbb1bcae2310
unknown <sharjeelabbas014@gmail.com>
2022-01-08 14:42:06 +0500

    Added Search bar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -14,15 +14,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late FloatingSearchBarController controller;
-
+  var recipe_url =
+      'https://api.edamam.com/api/recipes/v2?type=public&q=dynamic&app_id=63b50c42&app_key=23c73d1d368e716a3d8bb29c3cf2bf07';
   var apidata;
   void initState() {
-    apidata = API_Manager().getRecipe();
+    apidata = API_Manager().getRecipe(recipe_url);
 
     super.initState();
     controller = FloatingSearchBarController();
     dynamic filter2 = null;
-    filteredSearchHistory = filterSearchTerms(filter: filter2);
   }
 
   @override
@@ -31,72 +31,64 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
-/////////Search////////////
-
-  static const historyLength = 5;
-
-// The "raw" history that we don't access from the UI, prefilled with values
-  final List<String> _searchHistory = [
-    'fuchsia',
-    'flutter',
-    'widgets',
-    'resocoder',
-  ];
-// The filtered & ordered history that's accessed from the UI
-  late List<String> filteredSearchHistory;
-
-// The currently searched-for term
-  late String? selectedTerm;
-  List<String> filterSearchTerms({
-    required String filter,
-  }) {
-    // ignore: unnecessary_null_comparison
-    if (filter != null && filter.isNotEmpty) {
-      // Reversed because we want the last added items to appear first in the UI
-      return _searchHistory.reversed
-          .where((term) => term.startsWith(filter))
-          .toList();
-    } else {
-      return _searchHistory.reversed.toList();
-    }
-  }
-
-  void addSearchTerm(String term) {
-    if (_searchHistory.contains(term)) {
-      // This method will be implemented soon
-      putSearchTermFirst(term);
-      return;
-    }
-    _searchHistory.add(term);
-    if (_searchHistory.length > historyLength) {
-      _searchHistory.removeRange(0, _searchHistory.length - historyLength);
-    }
-    // Changes in _searchHistory mean that we have to update the filteredSearchHistory
-    dynamic filter2 = null;
-    filteredSearchHistory = filterSearchTerms(filter: filter2);
-  }
-
-  void deleteSearchTerm(String term) {
-    _searchHistory.removeWhere((t) => t == term);
-    dynamic filter2 = null;
-    filteredSearchHistory = filterSearchTerms(filter: filter2);
-  }
-
-  void putSearchTermFirst(String term) {
-    deleteSearchTerm(term);
-    addSearchTerm(term);
-  }
-
   @override
   Widget build(BuildContext context) {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Rest API Code"),
+        title: Text("Recipe App"),
       ),
       body: Column(
         children: [
-          Container(
-            child: FutureBuilder<RecipeModel>(
+          SizedBox(
+            height: 72,
+            child: FloatingSearchBar(
+              hint: 'Search...',
+              scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+              transitionDuration: const Duration(milliseconds: 800),
+              transitionCurve: Curves.easeInOut,
+              physics: const BouncingScrollPhysics(),
+              axisAlignment: isPortrait ? 0.0 : -1.0,
+              openAxisAlignment: 0.0,
+              width: isPortrait ? 600 : 500,
+              debounceDelay: const Duration(milliseconds: 500),
+              onQueryChanged: (query) {
+                setState(() {
+                  recipe_url =
+                      'https://api.edamam.com/api/recipes/v2?type=public&q=' +
+                          query +
+                          '&app_id=63b50c42&app_key=23c73d1d368e716a3d8bb29c3cf2bf07';
+                  if (query != "") {
+                    query = "dynamic";
+                  }
+                  apidata = API_Manager().getRecipe(recipe_url);
+                });
+              },
+              transition: CircularFloatingSearchBarTransition(),
+              actions: [
+                FloatingSearchBarAction(
+                  showIfOpened: false,
+                  child: CircularButton(
+                    icon: const Icon(Icons.place),
+                    onPressed: () {},
+                  ),
+                ),
+                FloatingSearchBarAction.searchToClear(
+                  showIfClosed: false,
+                ),
+              ],
+              builder: (context, transition) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<RecipeModel?>(
               future: apidata,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
@@ -136,154 +128,15 @@ class _HomePageState extends State<HomePage> {
               },
             ),
           ),
+
+          //yeh child masla hai mtlb k i don't know how to integrate kuch b
         ],
-
-        //yeh child masla hai mtlb k i don't know how to integrate kuch b
-
-        child: FloatingSearchBar(
-          controller: controller,
-          body: FloatingSearchBarScrollNotifier(
-            child: SearchResultListView(
-              searchTerm: selectedTerm,
-              key: null,
-            ),
-          ),
-          transition: CircularFloatingSearchBarTransition(),
-          physics: const BouncingScrollPhysics(),
-          title: Text(
-            selectedTerm ?? 'Search Recipe',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          hint: 'Search and find out...',
-          actions: [
-            FloatingSearchBarAction.searchToClear(),
-          ],
-          onQueryChanged: (query) {
-            setState(() {
-              filteredSearchHistory = filterSearchTerms(filter: query);
-            });
-          },
-          onSubmitted: (query) {
-            setState(() {
-              addSearchTerm(query);
-              selectedTerm = query;
-            });
-            controller.close();
-          },
-          builder: (context, transition) {
-            return ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Material(
-                  color: Colors.white,
-                  elevation: 4,
-                  child: Builder(builder: (context) {
-                    if (filteredSearchHistory.isEmpty &&
-                        controller.query.isEmpty) {
-                      return Container(
-                        height: 56,
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Start Searching',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: Theme.of(context).textTheme.caption,
-                        ),
-                      );
-                    } else if (filteredSearchHistory.isEmpty) {
-                      return ListTile(
-                        title: Text(controller.query),
-                        leading: const Icon(Icons.search),
-                        onTap: () {
-                          setState(() {
-                            addSearchTerm(controller.query);
-                            selectedTerm = controller.query;
-                          });
-                          controller.close();
-                        },
-                      );
-                    } else {
-                      return Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: filteredSearchHistory
-                            .map(
-                              (term) => ListTile(
-                                title: Text(
-                                  term,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                leading: const Icon(Icons.history),
-                                trailing: IconButton(
-                                  icon: const Icon(Icons.clear),
-                                  onPressed: () {
-                                    setState(() {
-                                      deleteSearchTerm(term);
-                                    });
-                                  },
-                                ),
-                                onTap: () {
-                                  setState(() {
-                                    putSearchTermFirst(term);
-                                    selectedTerm = term;
-                                  });
-                                  controller.close();
-                                },
-                              ),
-                            )
-                            .toList(),
-                      );
-                    }
-                  }),
-                ));
-          },
-        ),
-        //]
       ),
     );
   }
 }
 
-class SearchResultListView extends StatelessWidget {
-  final String? searchTerm;
-  const SearchResultListView({
-    required Key? key,
-    required this.searchTerm,
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    if (searchTerm == null) {
-      return Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Icon(
-              Icons.search,
-              size: 64,
-            ),
-            Text(
-              'Start Searching',
-              style: Theme.of(context).textTheme.headline5,
-            )
-          ],
-        ),
-      );
-    }
-    final fsb = FloatingSearchBar.of(context);
-
-    return ListView(
-      padding: const EdgeInsets.only(top: 1.0),
-      children: List.generate(
-        50,
-        (index) => ListTile(
-          title: Text('$searchTerm search result'),
-          subtitle: Text(index.toString()),
-        ),
-      ),
-    );
-  }
-}
 //-------------------------------------------------------------------------------------------
 //this the first running code that takes data from the API and shows it
 /*
--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -4,11 +4,11 @@ import 'package:rest_api/constants/strings.dart';
 import 'package:rest_api/models/recipeinfo.dart';
 
 class API_Manager {
-  Future<RecipeModel> getRecipe() async {
+  Future<RecipeModel?> getRecipe(String recipe_url) async {
     var recipedatamodel;
     try {
       var client = http.Client();
-      var url = Uri.parse(Strings.recipe_url);
+      var url = Uri.parse(recipe_url);
       var response = await client.get(url, headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
@@ -17,6 +17,9 @@ class API_Manager {
         //status = "success";
         var jsonstring = response.body;
         var jsonMap = json.decode(jsonstring);
+        if (jsonMap == null) {
+          return null;
+        }
         recipedatamodel = RecipeModel.fromJson(jsonMap);
         print(recipedatamodel.hits);
       } else {
